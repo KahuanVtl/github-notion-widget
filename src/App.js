ac7aefedcbfee6ae240f80baa1b372eb382a5510
kahuan <kahuanvitelli@gmail.com>
2024-08-27 23:34:07 -0300

    Arrumando Relogio.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import Relogio from './relogio.js';
 
-function Relogio() {
-  const [horarioAtual, setHorarioAtual] = useState(new Date());
-
-  useEffect(() => {
-    const timerID = setInterval(() => updateTime(), 1000);
-
-    return function cleanup() {
-      clearInterval(timerID);   
-    };
-  }, []);
-
-  function updateTime() {
-    setHorarioAtual(new Date());
-  }
-
+function App() {
   return (
-    <div className='clock-container'>
-      <h2>{horarioAtual.toLocaleTimeString()}</h2>
+    <div className="App">
+      <Relogio />
     </div>
   );
 }
 
-export default Relogio;
+export default App;
